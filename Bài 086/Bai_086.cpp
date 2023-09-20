831ad624080b3dc308da087b0a818715faf5155f
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-09-20 12:10:36 +0700

    84-88

---
+++ Bài 086/Bai_086.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "Nhap x: ";
+	cin >> x;
+
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+
+	float s = 0;
+	float t = 1;
+	int i = 2;
+	int dau = -1;
+
+	while (i <= 2*n)
+	{
+		t = t * x * x;
+		s = s + dau * t;
+		i = i + 2;
+		dau = -dau;
+	}
+	cout << "s = " << s;
+	return 0;
+}
