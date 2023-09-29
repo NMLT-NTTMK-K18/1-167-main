603fcbb93e65a6d2d66a3e8af7900b02351e37bf
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-09-29 22:57:59 +0700

    🚑 Fix 96 i = 1

--- Bai096/Source.cpp
+++ Bai096/Source.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -10,7 +10,7 @@ int main()
 
 	float s = 0;
 	int t = 1;
-	int i = n;
+	int i = 1;
 
 	while (i <= n)
 	{
